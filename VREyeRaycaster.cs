b3724d061a76fb0464d715239a4dc53c78862d92
ctmccurd <ctmccurd@ncsu.edu>
2016-09-18 12:13:44 -0400

    Add files via upload
    Access to my demo

---
+++ VREyeRaycaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace VRStandardAssets.Utils
+{
+    // In order to interact with objects in the scene
+    // this class casts a ray into the scene and if it finds
+    // a VRInteractiveItem it exposes it for other classes to use.
+    // This script should be generally be placed on the camera.
+    public class VREyeRaycaster : MonoBehaviour
+    {
+        public event Action<RaycastHit> OnRaycasthit;                   // This event is called every frame that the user's gaze is over a collider.
+    }
+}
