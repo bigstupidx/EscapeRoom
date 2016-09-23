bb0d2eb7213e9a9bb10a7d1c9dfc28a52159c783
mynameisSleepy <gson1394@gmail.com>
2016-09-23 01:18:52 -0400

    recording menu progress
    The scene loading makes the recording not play. Need to signal the new
    scene to play recordings

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
-
+    public RecordingManager recordingManager;
 
 	public void PlayButtonPressed()
     {
@@ -28,5 +28,11 @@ public class MenuManager : MonoBehaviour {
     public void MainMenuButtonPressed()
     {
         Application.LoadLevel("MenuScene");
+    }
+
+    public void ReplayRecordingButtonPressed()
+    {
+        Application.LoadLevel("GameScene");
+        recordingManager.LoadRecordingButtonPressed();
     }
 }
