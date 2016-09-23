c3ee4e71c4fce647dd22a2d8869473e270b5207f
mynameisSleepy <gson1394@gmail.com>
2016-09-23 00:15:05 -0400

    Attempted back button
    There seems to be an issue with loading the scenes that is causing the
    back button to not load the main menu properly

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,19 +5,28 @@ public class MenuManager : MonoBehaviour {
 
 
 
-	public void PlayButtonPressed() {
+	public void PlayButtonPressed()
+    {
 		Application.LoadLevel ("GameScene");
 	}
 
-	public void RecordingsButtonPressed() {
+	public void RecordingsButtonPressed()
+    {
         Application.LoadLevel("RecordingsMenuScene");
     }
 
-	public void SettingsButtonPressed() {
+	public void SettingsButtonPressed()
+    {
 		Application.LoadLevel ("SettingsMenuScene");
 	}
 
-	public void QuitButtonPressed() {
+	public void QuitButtonPressed()
+    {
 		Application.Quit();
 	}
+
+    public void MainMenuButtonPressed()
+    {
+        Application.LoadLevel("MainMenuScene");
+    }
 }
