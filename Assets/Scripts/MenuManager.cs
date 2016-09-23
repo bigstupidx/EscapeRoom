ebcd3321667d16cb563845772f0c982a8e54c8ec
gcjones <gcjones@ncsu.edu>
2016-09-22 20:46:52 -0400

    Initial menus commit
    scenes for the main menu
    recordings menu
    settings menu
    and a blank test scene
    recording, play, and quit buttons are currently working

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+
+
+	public void PlayButtonPressed() {
+		Application.LoadLevel ("TestScene");
+	}
+
+	void RecordingsButtonPressed() {
+
+	}
+
+	public void SettingsButtonPressed() {
+		Application.LoadLevel ("SettingsMenuScene");
+	}
+
+	public void QuitButtonPressed() {
+		Application.Quit();
+	}
+}
