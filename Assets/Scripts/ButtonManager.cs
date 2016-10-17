30124eeb06dab112f39b28608c9c7fe8917cec25
CMac59 <ctmccurd@ncsu.edu>
2016-10-17 10:35:18 -0400

    Button stuff

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+    public GameObject copy;
+    public void rotateUP()
+    {
+        copy.transform.rotation.Set(copy.transform.rotation.x, 90, copy.transform.rotation.z, copy.transform.rotation.w);
+    }
+}
