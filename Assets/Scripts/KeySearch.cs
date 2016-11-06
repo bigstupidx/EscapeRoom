d97abaf98bc602deaf18ad91709d497248ac83f2
CMac59 <ctmccurd@ncsu.edu>
2016-11-06 00:03:16 -0400

    Finished the Ice puzzle.

---
+++ Assets/Scripts/KeySearch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class KeySearch : Searchable{
+
+    // Use this for initialization
+
+    public GameObject key;
+    KeySearch()
+    {
+        message = "You found the Key!";
+    }
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+
+        key.transform.position = new Vector3(3.0009f, .659f, -.84f);
+    }
+}
