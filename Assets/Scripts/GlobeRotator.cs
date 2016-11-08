9075a1bf6cf8fc255baba99bab49e45c317661da
shonferg <me@shonf.com>
2016-11-07 21:26:14 -0500

    More progress on light puzzle
    Created clickable country meshes for 3 countries on the globe.
    Allowed the user to rotate the globe by clicking outside any of the
    countries.

---
+++ Assets/Scripts/GlobeRotator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GoalMover))]
+public class GlobeRotator : Focusable {
+
+	public GoalMover otherHalf;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerClick(eventData);
+
+		// Rotate both half of the globe by 45 degrees
+		GoalMover thisHalf = GetComponent<GoalMover>();
+
+		Vector3 newRotation = transform.rotation.eulerAngles;
+		newRotation.y += 45;
+
+		thisHalf.AddGoal(transform.position, Quaternion.Euler(newRotation));
+		otherHalf.AddGoal(otherHalf.transform.position, Quaternion.Euler(newRotation));
+	}
+}
