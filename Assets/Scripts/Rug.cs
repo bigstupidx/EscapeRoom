02a81677ea99bfb8d03a072f32bb4cd12ba0e2c2
shonferg <me@shonf.com>
2016-11-11 23:10:45 -0500

    Created carpet curling animation and script to activate it

---
+++ Assets/Scripts/Rug.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rug : Focusable {
+
+	protected Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+	}
+
+	public override void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		// Play the animation
+		anim.Play();
+
+		// Change layer of this object so it is no longer selectable
+		gameObject.layer = 2;
+	}
+}
