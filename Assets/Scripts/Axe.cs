fe08accd61cd873916770bd01e93e93e5177a571
CMac59 <ctmccurd@ncsu.edu>
2016-11-16 09:46:15 -0500

    Ice puzzle stuff

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Axe : Pickup {
+    // Use this for initialization
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (!IsFound)
+        {
+            base.OnPointerClick(eventData);
+        }
+    }
+
+    public bool checkClick()
+    {
+        return IsFound;
+    }
+}
---
+++ Assets/Scripts/Chair.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Chair : Searchable {
+    public Axe axe;
+    public GameObject InventoryPosition;
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (axe.checkClick())
+        {
+            GoalMover mover = gameObject.GetComponent<GoalMover>();
+            mover.ClearGoals();
+            mover.AddGoal(InventoryPosition.transform.position, InventoryPosition.transform.rotation);
+        }
+        else
+        {
+            base.OnPointerClick(eventData);
+        }
+    }
+
+    
+}
