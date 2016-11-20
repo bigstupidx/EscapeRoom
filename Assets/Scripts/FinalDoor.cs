4fc7fe5d8f962c3168b7fc3daeaab531cafb80e6
gdmannin <gdmannin@ncsu.edu>
2016-11-20 14:14:07 -0500

    stats & winscene changes

--- Assets/Scripts/FinalDoor.cs
+++ Assets/Scripts/FinalDoor.cs
@@ -9,22 +9,25 @@ public class FinalDoor : Lockable {
 	public override bool CanUnlock()
 	{
 		// Only let the player escape when all keys are found
-		foreach (Pickup key in keys) {
+		/*foreach (Pickup key in keys) {
 			if (!key.IsFound) {
 				return false;
 			}
-		}
+		}*/
 
 		if (RecordingManager.State == RecordingState.Recording) {
-			// Stop recording
-			RecordingManager.StopRecording();
-
-			// Save the recording into the static variable of the MenuManager
-			MenuManager.SavedRecording = RecordingManager.GetRecordingFromActiveScene();
-
 			// Load the win scene
 			SceneManager.LoadScene("WinScene");
-		}
+
+            // Stop recording
+            RecordingManager.StopRecording();
+
+            //Calculate WinScene stats
+            Statistics.Stop();
+
+            // Save the recording into the static variable of the MenuManager
+            MenuManager.SavedRecording = RecordingManager.GetRecordingFromActiveScene();
+        }
 
 		return true;
 	}
