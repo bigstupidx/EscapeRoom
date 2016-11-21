c14cac446a2cc725e2b14bf7ad9c58407cb1442f
CMac59 <ctmccurd@ncsu.edu>
2016-11-21 11:29:39 -0500

    Revert "Pushing my puzzle to the final scene"
    This reverts commit d4cf4ed99b1d2531911d08d375e3d8779fbaf88c.

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -5,8 +5,6 @@ using UnityEngine.EventSystems;
 public class Log : Searchable {
     public GameObject campfire;
     public GameObject matchbox;
-    public GameObject Pos1;
-    public bool IsFound = false;
     public Log()
     {
         message = "The log lights on fire!";
@@ -18,39 +16,26 @@ public class Log : Searchable {
 
     public override void OnPointerClick(PointerEventData eventData)
     {
-        if (IsFound)
+        if (!campfire.activeSelf)
         {
-            if (!campfire.activeSelf)
-            {
-                if (matchbox.GetComponent<Pickup>().IsFound)
+            if (matchbox.GetComponent<Pickup>().IsFound) {
+                base.OnPointerClick(eventData);
+                campfire.SetActive(true);
+                foreach (ParticleSystem ps in campfire.GetComponentsInChildren<ParticleSystem>())
                 {
-                    base.OnPointerClick(eventData);
-                    campfire.SetActive(true);
-                    foreach (ParticleSystem ps in campfire.GetComponentsInChildren<ParticleSystem>())
-                    {
-                        ps.Play();
-                    }
-                }
-                else
-                {
-                    message = "How did this come from a chair???";
-                    base.OnPointerClick(eventData);
+                    ps.Play();
                 }
             }
             else
             {
-                message = "That fire looks a little too hot to touch";
+                message = "How did this come from a chair???";
                 base.OnPointerClick(eventData);
             }
         }
         else
         {
-            IsFound = true;
-            message = "You add the log to the oven.";
+            message = "That fire looks a little too hot to touch";
             base.OnPointerClick(eventData);
-            GoalMover mover = GetComponent<GoalMover>();
-            mover.ClearGoals();
-            mover.AddGoal(Pos1.transform.position, Pos1.transform.rotation);
         }
     }
 }
